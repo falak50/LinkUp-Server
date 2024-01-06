775b058f521fdfeb1700546df010f37150681479
falak50 <83400991+falak50@users.noreply.github.com>
2024-01-06 10:17:46 -1200

    loading deppend on backend userinfo intro info data work done

--- index.js
+++ index.js
@@ -26,26 +26,101 @@ async function run() {
       // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
       
-      const menuC=client.db("bistroDb").collection("menu");
-      const usersCollection=client.db("LinkUp").collection("users");
+      //const menuC=client.db("bistroDb").collection("menu");
+      const userCollection=client.db("LinkUp").collection("users");
       //user related apis
       app.post('/users',async(req,res)=>{
         console.log("in side the backend /users")
         const user = req.body;
         console.log(user);
         const query = {email:user.email};
-        const existingUser = await usersCollection.findOne(query);
+        const existingUser = await userCollection.findOne(query);
         // console.log('existing user',existingUser);
         if(existingUser){
           return res.send({message:'user already exists'});
         }
-        const result = await usersCollection.insertOne(user);
+        const result = await userCollection.insertOne(user);
         res.send(result);
   
       });
+
       
+      // app.get('/users', async (req, res) => {
+      //   //  const result = await userCollection.find().toArray();
+        
+      //    res.send(result);
+      // })
 
+      app.get('/users',async(req,res)=>{
+        const result = await userCollection.find().toArray();
+        res.send(result);
+      })
 
+      app.get('/users/:email', async (req, res) => {
+        const query = { email: req.params.email };
+        const user = await userCollection.findOne(query);
+        // const user =req.params.email;  
+        res.send(user);
+      })
+      app.patch('/users/:id',async(req,res)=>{
+        console.log('log in')
+        const id=req.params.id;
+             const { first_name, last_name,additional_name,headline,education,country,city} = req.body;
+             console.log(req.body)
+        console.log('user admin id => ',id);
+        const filter = {_id:new ObjectId(id)};
+        const updateDoc = {
+          $set: {
+                  first_name: first_name,
+                  last_name: last_name,
+                  additional_name: additional_name,
+                  headline: headline,
+                  education: education,
+                  country: country,
+                  city: city,
+                  ok:'not ok 1'
+          },
+        };
+  
+        const result = await userCollection.updateOne(filter,updateDoc);
+        res.send(result);
+        
+      })
+      // app.post('/users/:email', async (req, res) => {
+      //   console.log('in email  post')
+      //   const emailToUpdate = req.params.email;
+      //   console.log("email check ",emailToUpdate)
+      //   const { first_name, last_name,additional_name,headline,education,country,city} = req.body;
+      //   console.log(req.body);
+  
+      //     userCollection.updateOne(
+      //       { email: emailToUpdate },
+      //       {
+      //         $set: {
+      //           first_name: first_name,
+      //           last_name: last_name,
+      //           additional_name: additional_name,
+      //           headline: headline,
+      //           education: education,
+      //           country: country,
+      //           city: city,
+      //         },
+      //       }
+      //     ).then(res=>{
+
+      //       console.log('res',res)
+
+      //     });
+      //     // console.log(result);
+  
+      //     // if (result.matchedCount === 0) {
+      //     //   return res.status(404).send({ message: 'User not found' });
+      //     // }
+      //     // console.log('res.send({ message: ')
+      //     res.send({ message: 'User updated successfully'});
+      //   } )
+      
+      
        //user related apis
       app.get('/menu',async(req,res)=>{
         const result = "falak bai is a boosss"
@@ -83,3 +158,5 @@ app.listen(port,()=>{
  * app.put('users/:id') // create and update if already have only update
  * app.delete('users/:id')
 */
+
+
